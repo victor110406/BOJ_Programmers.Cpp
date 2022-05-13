b89ffba6869da57f88b1e93eefe05f3f04d8b99f
victor110406 <77650138+victor110406@users.noreply.github.com>
2022-05-13 14:17:31 +0900

    [Bronze II] Title: 가장 많은 글자, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1371. 가장 많은 글자/가장 많은 글자.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+
+int alpha[26];
+
+int main()
+{
+	string str[50];
+	int alpha[26] = {};
+	int max = 0;
+
+	for (int i = 0; i < 50; i++) {
+		getline(cin, str[i]);
+
+		for (int j = 0; j < str[i].size(); j++) {
+			alpha[str[i][j] - 97]++;
+		}
+	}
+	for (int i = 0; i < 26; i++) {
+		if (max <= alpha[i]) {
+			max = alpha[i];
+		}
+	}
+	for (int i = 0; i < 26; i++) {
+		if (alpha[i] == max) {
+			cout << char(i + 97);
+		}
+	}
+}
