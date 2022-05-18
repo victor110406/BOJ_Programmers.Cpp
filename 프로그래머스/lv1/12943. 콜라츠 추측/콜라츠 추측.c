c1106223dd280cef7bda15f27d7e8c664b2999b0
victor110406 <77650138+victor110406@users.noreply.github.com>
2022-05-18 15:41:34 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.01 ms, Memory: 4.49 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num) {
+    int answer = 0;
+    unsigned long long num1 = num;
+    while(num1 != 1){
+        if(answer == 500){
+            return -1;
+            break;
+        }
+        if(num1 % 2 == 0){
+            num1 /= 2;
+            answer++;
+        }
+        else if(num1 % 2 == 1){
+            num1 *= 3;
+            num1 += 1;
+            answer++;
+        }
+    }
+    return answer;
+}
