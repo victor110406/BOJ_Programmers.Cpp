f159f6a2f429397896733919622852e25edf5784
victor110406 <77650138+victor110406@users.noreply.github.com>
2022-05-22 14:30:44 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    long long answer = 0, temp = 1;
+    int result = 0;
+    int i = 0, size = 1; 
+    while (n >= size) {
+        i++;
+        size *= 3;
+    }
+    size /= 3;
+    for (int j = 0; j < i; j++) {
+        if (j != 0) {
+            temp *= 10;
+        }
+        answer *= 10;
+        answer += (n / size);
+        n = (n % size);
+        size /= 3;
+    }
+
+    size++;
+
+    while (answer > 0) {
+        result += (answer / temp) * size;
+        answer %= temp;
+        temp /= 10;
+        size *= 3;
+    }
+    
+    return result;
+}
