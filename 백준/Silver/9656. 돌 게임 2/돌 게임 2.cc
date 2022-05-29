19070907264e60cb34f55f381a5e8df44c0a7828
victor110406 <77650138+victor110406@users.noreply.github.com>
2022-05-29 12:36:59 +0900

    [Silver IV] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int N;
+	 cin >> N;
+
+	 if (N % 2 == 1) {
+		 cout << "CY";
+	 } 
+	 else {
+		 cout << "SK";
+	 } 
+
+	 return 0; 
+}
