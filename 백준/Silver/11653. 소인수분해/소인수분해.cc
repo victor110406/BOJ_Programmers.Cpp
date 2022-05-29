8ca2474c8ec0c096aae14667a2f3553cf9ca7b8f
victor110406 <77650138+victor110406@users.noreply.github.com>
2022-05-29 12:38:12 +0900

    [Silver V] Title: 소인수분해, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	cin >> a;
+	for (int i = 2; i <= a; i++) {
+		if (a % i == 0) {
+			cout << i << endl;
+			a = a / i;
+			i = 1;
+		}
+	}
+}
