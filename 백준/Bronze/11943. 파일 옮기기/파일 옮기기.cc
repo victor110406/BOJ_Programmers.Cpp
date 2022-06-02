c4a265bdb2f913c909579b6855c296ccbb6c8dbd
victor110406 <77650138+victor110406@users.noreply.github.com>
2022-06-02 20:26:44 +0900

    [Bronze IV] Title: 파일 옮기기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11943. 파일 옮기기/파일 옮기기.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+		
+	int a, b, c, d;
+	cin >> a >> b >> c >> d;
+	if (a + d > b + c) {
+		cout << b + c;
+	}
+	else {
+		cout << a + d;
+	}
+	return 0;
+}
