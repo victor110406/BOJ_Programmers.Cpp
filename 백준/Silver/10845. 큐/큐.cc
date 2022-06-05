bd3edc28ccd1762a3780b36460dd0eb9326fae4c
victor110406 <77650138+victor110406@users.noreply.github.com>
2022-06-05 12:31:57 +0900

    [Silver IV] Title: 큐, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.cc
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <deque>
+#include <string>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	deque<int> dq;
+	int N, a;
+	string str;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cin >> str;
+		if (str == "push") {
+			cin >> a;
+			dq.push_back(a);
+		}
+		else if (str == "pop") {
+			if (dq.empty()) {
+				cout << -1 << '\n';
+			}
+			else {
+				cout << dq.front() << '\n';
+				dq.pop_front();
+			}
+		}
+		else if (str == "front") {
+			if (dq.empty()) {
+				cout << -1 << '\n';
+			}
+			else {
+				cout << dq.front() << '\n';
+			}
+		}
+		else if (str == "back") {
+			if (dq.empty()) {
+				cout << -1 << '\n';
+			}
+			else {
+				cout << dq.back() << '\n';
+			}
+		}
+		else if (str == "size") {
+			cout << dq.size() << '\n';
+		}
+		else if (str == "empty") {
+			cout << dq.empty() << '\n';
+		}
+		
+	}
+	return 0;
+}
