8bfa9db6af7c2897196609607160686cd4a3a903
victor110406 <77650138+victor110406@users.noreply.github.com>
2022-06-07 12:38:42 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    int temp;
+    while (b != 0)
+    {
+        temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
+int lcm(int a,int b)
+{
+    return a * b / gcd(a, b);
+}
+
+vector<int> solution(int n, int m) {
+    vector<int> answer;
+    answer.push_back(gcd(n, m));
+    answer.push_back(lcm(n, m));
+    return answer;
+}
