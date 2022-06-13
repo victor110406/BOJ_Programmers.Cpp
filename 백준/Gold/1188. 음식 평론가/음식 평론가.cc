8b02dda5e6557a386ad87cd494ba25b11f79e378
victor110406 <77650138+victor110406@users.noreply.github.com>
2022-06-13 12:40:12 +0900

    [Gold IV] Title: 음식 평론가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1188. 음식 평론가/음식 평론가.cc
+++ 백준/Gold/1188. 음식 평론가/음식 평론가.cc
@@ -2,9 +2,8 @@
 using namespace std;
 
 int gcd(int a, int b) {
-	int temp;
 	while (b != 0) {
-		temp = a % b;
+		int temp = a % b;
 		a = b;
 		b = temp;
 	}
@@ -14,7 +13,6 @@ int gcd(int a, int b) {
 int main() {
 	int N, M;
 	cin >> N >> M;
-	cout << M - gcd(N, M) << "\n";
-
+	cout << M - gcd(N, M) << '\n';
 	return 0;
 }
