336d0d6ab44b6b2574d522a4814d9577307092f3
victor110406 <77650138+victor110406@users.noreply.github.com>
2022-06-16 19:43:08 +0900

    [Bronze III] Title: 2의 제곱인가?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11966. 2의 제곱인가？/2의 제곱인가？.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 1;
+    cin >> n;
+    if (n == 1) {
+        cout << "1" << '\n';
+        return 0;
+    }
+    while (sum < n) {
+        sum *= 2;
+    }
+    if (sum == n) {
+        cout << 1 << '\n';
+    }
+    else {
+        cout << 0 << '\n';
+    }
+    return 0;
+}
