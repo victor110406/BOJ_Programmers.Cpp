7ae2620937f81d9070072425a5290ec71b21697c
Wonsan Hong <victor110406@nate.com>
2022-07-03 12:10:25 +0900

    [Bronze IV] Title: 카드 뽑기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16204. 카드 뽑기/카드 뽑기.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	int N, M, K;
+	cin >> N >> M >> K;
+	cout << min(M, K) + min(N - M, N - K) << '\n';
+
+	return 0;
+}
