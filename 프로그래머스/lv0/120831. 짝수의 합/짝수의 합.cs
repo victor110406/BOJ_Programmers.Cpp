0cca2559a0e3b955669df78e7e20b424e077b077
Wonsan Hong <victor110406@nate.com>
2022-10-28 11:56:56 +0900

    [level 0] Title: 짝수의 합, Time: 0.24 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int result = 0;
+        for(int i=1; i<=n; i++){
+            if(i % 2 == 0){
+            result += i;
+        }
+    }
+    return result;
+    }
+}
