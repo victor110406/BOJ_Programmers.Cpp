55d737be817e1350fab9de794710588aea9e10a3
Wonsan Hong <victor110406@nate.com>
2022-12-03 13:35:55 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.13 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        if(n % 7 == 0){
+            return n / 7;
+        }
+        else{
+            return n / 7 + 1;
+        }
+    }
+}
