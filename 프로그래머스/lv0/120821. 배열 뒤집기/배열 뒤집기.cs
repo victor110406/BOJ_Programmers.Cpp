239d5fd7fa6f6c552d20a359e153367b566608a9
Wonsan Hong <victor110406@nate.com>
2023-02-11 13:28:25 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.33 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120821. 배열 뒤집기/배열 뒤집기.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Solution {
+    public int[] solution(int[] num_list) {
+        Array.Reverse(num_list);
+        int[] answer = num_list;
+        return answer;
+    }
+}
