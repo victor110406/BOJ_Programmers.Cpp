0d5715a0b2eaf8c5b872857e3c51f48bacec2210
Wonsan Hong <victor110406@nate.com>
2023-02-26 18:38:44 +0900

    [level 0] Title: 배열의 유사도, Time: 0.18 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution {
+    public int solution(string[] s1, string[] s2) {
+       int answer = 0;
+        for(int i = 0; i < s1.Length; i++)
+        {
+            for(int j = 0; j < s2.Length; j++)
+            {
+                if(s1[i].Equals(s2[j]))
+                {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
