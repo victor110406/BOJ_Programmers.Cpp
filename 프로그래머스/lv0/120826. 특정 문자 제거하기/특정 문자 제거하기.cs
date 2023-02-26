2a7e062530d03c1de31d057565b6de37f6e6c7bb
Wonsan Hong <victor110406@nate.com>
2023-02-26 17:37:44 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.16 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.cs
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.cs
@@ -2,7 +2,6 @@ using System;
 
 public class Solution {
     public string solution(string my_string, string letter) {
-        my_string = my_string.Replace(letter, string.Empty);
-        return my_string;
+        return my_string = my_string.Replace(letter, string.Empty);
     }
 }
