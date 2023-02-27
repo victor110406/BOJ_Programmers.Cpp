3978330ac82a4eec8ed3e2f269588e52e0a04c7d
Wonsan Hong <victor110406@nate.com>
2023-02-27 19:22:04 +0900

    [level 0] Title: 개미 군단, Time: 0.16 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+        if(hp >= 5){
+            answer += hp / 5;
+            hp %= 5;
+        }
+        if(hp >= 3){
+            answer += hp / 3;
+            hp %= 3;
+        }
+        if(hp >= 1){
+            answer += hp / 1;
+            hp %= 1;
+        }
+        return answer;
+    }
+}
