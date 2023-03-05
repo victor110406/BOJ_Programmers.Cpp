c48b58096692fef24a3d0e7df43e537d665dd6b4
Wonsan Hong <victor110406@nate.com>
2023-03-05 16:43:53 +0900

    [level 0] Title: 369게임, Time: 0.16 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        while(order > 0){
+            if((order % 10) % 3 == 0 && order % 10 != 0){
+                answer++;
+                order /= 10;
+            }
+            else{
+                order /= 10;
+            }
+        }
+        return answer;
+    }
+}
