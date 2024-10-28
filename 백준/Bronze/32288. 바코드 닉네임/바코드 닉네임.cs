6d6454ad1d5e829b4ec998c6075b079e9df88f52
Wonsan Hong <victor110406@nate.com>
2024-10-28 13:15:53 +0900

    [Bronze IV] Title: 바코드 닉네임, Time: 44 ms, Memory: 5384 KB -BaekjoonHub

---
+++ 백준/Bronze/32288. 바코드 닉네임/바코드 닉네임.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            Console.WriteLine(Console.ReadLine().Replace('I', 'i').Replace('l','L'));
+        }
+    }
+}
