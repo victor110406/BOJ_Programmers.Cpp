8c87bff8900bd67bef8a39f18f96ed5c452fb668
Wonsan Hong <victor110406@nate.com>
2024-10-28 13:23:55 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 48 ms, Memory: 5468 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Programmers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            for (int i=0; i<int.Parse(input); i++)
+            {
+                Console.Write("LoveisKoreaUniversity ");
+            }
+        }
+    }
+}
