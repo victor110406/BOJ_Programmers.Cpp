7d5833e00daf236d7b9792d3515669366020a448
Wonsan Hong <victor110406@nate.com>
2024-10-31 13:10:56 +0900

    [Bronze V] Title: Hook, Time: 40 ms, Memory: 5320 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Programmers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#  # #### #### #  #");
+            Console.WriteLine("#### #  # #  # # #");
+            Console.WriteLine("#### #  # #  # # #");
+            Console.WriteLine("#  # #### #### #  #");
+        }
+    }
+}
