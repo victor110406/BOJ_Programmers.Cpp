519808a66743257dcceb2b44b6dca37c11350b56
Wonsan Hong <victor110406@nate.com>
2024-11-03 02:07:17 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 48 ms, Memory: 5376 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Programmers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = Console.ReadLine();
+            if (result == "SONGDO")
+            {
+                Console.WriteLine("HIGHSCHOOL");
+            }
+            else if (result == "CODE")
+            {
+                Console.WriteLine("MASTER");
+            }
+            else if (result == "2023")
+            {
+                Console.WriteLine("0611");
+            }
+            else  if (result == "ALGORITHM")
+            {
+                Console.WriteLine("CONTEST");
+            }
+
+        }
+    }
+}
