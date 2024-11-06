84d0557c5c6c397a2ed0728f16586eeebf5d407e
Wonsan Hong <victor110406@nate.com>
2024-11-06 11:32:58 +0900

    [Bronze V] Title: 時間 (Hour), Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Programmers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n * 24);
+        }
+    }
+}
