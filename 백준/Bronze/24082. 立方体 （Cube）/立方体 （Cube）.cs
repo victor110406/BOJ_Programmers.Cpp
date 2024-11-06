60176d23981b12392c2a7223158d9ab7c296626d
Wonsan Hong <victor110406@nate.com>
2024-11-06 11:46:59 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 48 ms, Memory: 5476 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Programmers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a * a * a);
+        }
+    }
+}
