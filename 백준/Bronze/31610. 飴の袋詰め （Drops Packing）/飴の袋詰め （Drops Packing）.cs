00408b95f6af538f7cc74d8fbe0bce62bc9dd732
Wonsan Hong <victor110406@nate.com>
2024-11-06 11:49:20 +0900

    [Bronze V] Title: 飴の袋詰め (Drops Packing), Time: 48 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/31610. 飴の袋詰め （Drops Packing）/飴の袋詰め （Drops Packing）.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Programmers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine(a * b + c);
+        }
+    }
+}
