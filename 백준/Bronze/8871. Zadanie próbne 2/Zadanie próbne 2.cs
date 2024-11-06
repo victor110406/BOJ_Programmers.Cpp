205fed2367f4e9df2499a89de7ed20ae8a611d86
Wonsan Hong <victor110406@nate.com>
2024-11-06 12:06:55 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 48 ms, Memory: 5484 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Programmers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            Console.Write((a + 1) * 2 + " " + (a + 1) * 3);
+        }
+    }
+}
