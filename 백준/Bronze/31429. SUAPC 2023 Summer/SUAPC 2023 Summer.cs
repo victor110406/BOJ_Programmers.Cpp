a9c0bfa44c435abe54637616a556f8046d495fb3
Wonsan Hong <victor110406@nate.com>
2024-11-07 15:49:53 +0900

    [Bronze V] Title: SUAPC 2023 Summer, Time: 48 ms, Memory: 5368 KB -BaekjoonHub

---
+++ 백준/Bronze/31429. SUAPC 2023 Summer/SUAPC 2023 Summer.cs
@@ -0,0 +1,48 @@
+using System;
+
+namespace Programmers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            switch (a)
+            {
+                case "1":
+                    Console.WriteLine("12 1600");
+                    break;
+                case "2":
+                    Console.WriteLine("11 894");
+                    break;
+                case "3":
+                    Console.WriteLine("11 1327");
+                    break;
+                case "4":
+                    Console.WriteLine("10 1311");
+                    break;
+                case "5":
+                    Console.WriteLine("9 1004");
+                    break;
+                case "6":
+                    Console.WriteLine("9 1178");
+                    break;
+                case "7":
+                    Console.WriteLine("9 1357");
+                    break;
+                case "8":
+                    Console.WriteLine("8 837");
+                    break;
+                case "9":
+                    Console.WriteLine("7 1055");
+                    break;
+                case "10":
+                    Console.WriteLine("6 556");
+                    break;
+                case "11":
+                    Console.WriteLine("6 773");
+                    break;
+            }
+        }
+    }
+}
