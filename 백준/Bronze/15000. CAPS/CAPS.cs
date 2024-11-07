4180fd8f35b3bbe23f63adcdbc97b412b80151f5
Wonsan Hong <victor110406@nate.com>
2024-11-07 14:16:54 +0900

    [Bronze V] Title: CAPS, Time: 188 ms, Memory: 12448 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Programmers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            Console.WriteLine(a.ToUpper());
+        }
+    }
+}
