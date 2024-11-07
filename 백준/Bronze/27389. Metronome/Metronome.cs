5011740f8d796766db3642ecd3efa29bbb80cba8
Wonsan Hong <victor110406@nate.com>
2024-11-07 13:57:44 +0900

    [Bronze V] Title: Metronome, Time: 52 ms, Memory: 5504 KB -BaekjoonHub

---
+++ 백준/Bronze/27389. Metronome/Metronome.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Programmers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            double b = int.Parse(a) / 4.0;
+            Console.WriteLine(b);
+        }
+    }
+}
