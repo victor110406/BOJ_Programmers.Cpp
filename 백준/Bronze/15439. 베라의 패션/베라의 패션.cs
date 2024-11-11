848917d587dbd7b23089fac83695dbaf1f767905
Wonsan Hong <victor110406@nate.com>
2024-11-11 12:07:14 +0900

    [Bronze IV] Title: 베라의 패션, Time: 48 ms, Memory: 5460 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Linq;
+
+namespace Programmers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine(a * (a - 1));
+        }
+    }
+}
