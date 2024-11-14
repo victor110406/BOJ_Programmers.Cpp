70688c7da77b06f43869db6d477eebb47d9bec02
Wonsan Hong <victor110406@nate.com>
2024-11-14 13:22:24 +0900

    [Bronze IV] Title: 특별한 가지, Time: 48 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        Console.WriteLine( b / a * c);
+    }
+}
