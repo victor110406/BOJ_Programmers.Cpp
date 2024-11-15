7f0f38240a45df12dc5b71ea4a32c1aaf65ad33b
Wonsan Hong <victor110406@nate.com>
2024-11-15 11:23:27 +0900

    [Bronze IV] Title: 2 番目に大きい整数 (The Second Largest Integer), Time: 56 ms, Memory: 5768 KB -BaekjoonHub

---
+++ 백준/Bronze/20976. 2 番目に大きい整数 （The Second Largest Integer）/2 番目に大きい整数 （The Second Largest Integer）.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Array.Sort(a);
+
+        Console.WriteLine(a[1]);
+    }
+}
