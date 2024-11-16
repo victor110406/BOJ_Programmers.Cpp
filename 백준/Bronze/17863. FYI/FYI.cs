71e76b4c00788cab5df8ae63bf1d7449f0471873
Wonsan Hong <victor110406@nate.com>
2024-11-16 11:27:06 +0900

    [Bronze IV] Title: FYI, Time: 48 ms, Memory: 5424 KB -BaekjoonHub

---
+++ 백준/Bronze/17863. FYI/FYI.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string a = Console.ReadLine();
+        if (a.StartsWith("555"))
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
