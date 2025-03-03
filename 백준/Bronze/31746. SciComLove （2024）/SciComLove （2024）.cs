ebc7ab4f87455c974c86f56287c3bf7387731b4d
Wonsan Hong <victor110406@nate.com>
2025-03-03 13:08:49 +0900

    [Bronze IV] Title: SciComLove (2024), Time: 52 ms, Memory: 5476 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/31746. SciComLove （2024）/SciComLove （2024）.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N = int.Parse(Console.ReadLine());
+
+            if (N % 2 == 0) { Console.WriteLine("SciComLove"); }
+            else { Console.WriteLine("evoLmoCicS"); }
+        }   
+    }
+}
